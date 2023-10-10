2a61fff98908410ddc4043b061cf1de00f127ff2
momena <momena.salloum59@gmail.com>
2023-10-10 21:10:55 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(str = "roller-skate") {
+    return `This Saturday, I want to ${str}!`
+}
+// console.log(saturdayFun("bathe my dog"));
+// console.log(saturdayFun());
+function mondayWork(str = "go to the office") {
+    return `This Monday, I will ${str}.`
+}
+function wrapAdjective(flair = "*") {
+    return function (p = "special") {
+        return `You are ${flair}${p}${flair}!`
+
+    }
+}
+// console.log(wrapAdjective("%")("a dedicated programmer"));
+console.log(wrapAdjective()());
